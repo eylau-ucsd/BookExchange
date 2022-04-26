2db7c6b3f92a879a17ad475b2febd27f00b1c130
eylau-ucsd <eylau@ucsd.edu>
2022-04-26 16:57:01 -0700

    add sanity check for arg count in main

--- main.cpp
+++ main.cpp
@@ -28,6 +28,10 @@ std::vector<int> parseWishList (std::ifstream& input) {
 }
 
 int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        std::cout << "Invalid arguments" << std::endl;
+        return 1;
+    }
     std::ifstream input (std::string(argv[1]), std::ifstream::in);
     Graph* G;
     BookExchange BookEx = parseBookExchange(input, G);
